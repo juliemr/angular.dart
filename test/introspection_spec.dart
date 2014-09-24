01c0c9076e23db08ebd0ffa0974ba1115ef173df
Julie Ralph <ju.ralph@gmail.com>
2014-09-24 15:30:54 -0700

    WIP - make introspection's findBindings and findModels work as webdriver expects

--- test/introspection_spec.dart
+++ test/introspection_spec.dart
@@ -7,50 +7,88 @@ import 'dart:html';
 
 void main() {
   describe('introspection', () {
-    it('should retrieve ElementProbe', (TestBed _) {
-      _.compile('<div ng-bind="true"></div>');
-      ElementProbe probe = ngProbe(_.rootElement);
-      expect(probe.injector.get(Injector)).toBe(_.injector);
-      expect(ngInjector(_.rootElement).get(Injector)).toBe(_.injector);
-      expect(probe.directives[0] is NgBind).toBe(true);
-      expect(ngDirectives(_.rootElement)[0] is NgBind).toBe(true);
-      expect(probe.scope).toBe(_.rootScope);
-      expect(ngScope(_.rootElement)).toBe(_.rootScope);
+    describe('ngQuery', () {
+      toHtml(List list) => list.map((e) => e.outerHtml).join('');
+
+      it('should select elements using CSS selector', () {
+        var div = new Element.html('<div><p><span></span></p></div>');
+        var span = div.querySelector('span');
+        var shadowRoot = span.createShadowRoot();
+        shadowRoot.innerHtml = '<ul><li>stash</li><li>secret</li><ul>';
+
+        expect(toHtml(ngQuery(div, 'li'))).toEqual('<li>stash</li><li>secret</li>');
+        expect(toHtml(ngQuery(div, 'li', 'stash'))).toEqual('<li>stash</li>');
+        expect(toHtml(ngQuery(div, 'li', 'secret'))).toEqual('<li>secret</li>');
+        expect(toHtml(ngQuery(div, 'li', 'xxx'))).toEqual('');
+      });
+
+      it('should select elements in the root shadow root', () {
+        var div = new Element.html('<div></div>');
+        var shadowRoot = div.createShadowRoot();
+        shadowRoot.innerHtml = '<ul><li>stash</li><li>secret</li><ul>';
+        expect(toHtml(ngQuery(div, 'li'))).toEqual('<li>stash</li><li>secret</li>');
+      });
     });
 
-    toHtml(List list) => list.map((e) => e.outerHtml).join('');
+    ddescribe('ngProbe', () {
+      it('should retrieve ElementProbe from a node', (TestBed _) {
+        _.compile('<div ng-bind="true"></div>');
 
-    it('should select elements using CSS selector', () {
-      var div = new Element.html('<div><p><span></span></p></div>');
-      var span = div.querySelector('span');
-      var shadowRoot = span.createShadowRoot();
-      shadowRoot.innerHtml = '<ul><li>stash</li><li>secret</li><ul>';
+        ElementProbe probe = ngProbe(_.rootElement);
 
-      expect(toHtml(ngQuery(div, 'li'))).toEqual('<li>stash</li><li>secret</li>');
-      expect(toHtml(ngQuery(div, 'li', 'stash'))).toEqual('<li>stash</li>');
-      expect(toHtml(ngQuery(div, 'li', 'secret'))).toEqual('<li>secret</li>');
-      expect(toHtml(ngQuery(div, 'li', 'xxx'))).toEqual('');
-    });
+        expect(probe.injector.get(Injector)).toBe(_.injector);
+        expect(ngInjector(_.rootElement).get(Injector)).toBe(_.injector);
+        expect(probe.directives[0] is NgBind).toBe(true);
+        expect(ngDirectives(_.rootElement)[0] is NgBind).toBe(true);
+        expect(probe.scope).toBe(_.rootScope);
+        expect(ngScope(_.rootElement)).toBe(_.rootScope);
+      });
 
-    it('should select probe using CSS selector', (TestBed _) {
-      _.compile('<div ng-show="true">WORKS</div>');
-      document.body.append(_.rootElement);
-      var div = new Element.html('<div><p><span></span></p></div>');
-      var span = div.querySelector('span');
-      var shadowRoot = span.createShadowRoot();
-      shadowRoot.innerHtml = '<ul><li>stash</li><li>secret</li><ul>';
+      it('should select probe using CSS selector', (TestBed _) {
+        _.compile('<div ng-show="true">WORKS</div>');
+        document.body.append(_.rootElement);
 
-      ElementProbe probe = ngProbe('[ng-show]');
-      expect(probe).toBeDefined();
-      expect(probe.injector.get(NgShow) is NgShow).toEqual(true);
-      _.rootElement.remove();
-    });
+        ElementProbe probe = ngProbe('[ng-show]');
 
-    it('should select elements in the root shadow root', () {
-      var div = new Element.html('<div></div>');
-      var shadowRoot = div.createShadowRoot();
-      shadowRoot.innerHtml = '<ul><li>stash</li><li>secret</li><ul>';
-      expect(toHtml(ngQuery(div, 'li'))).toEqual('<li>stash</li><li>secret</li>');
+        expect(probe).toBeDefined();
+        expect(probe.injector.get(NgShow) is NgShow).toEqual(true);
+        _.rootElement.remove();
+      });
+
+      it('elements should list binding expressions from child nodes', (TestBed _) {
+        _.compile('<div>{{a}}<span>{{b}}</span></div>');
+        _.rootScope.apply(() {
+          _.rootScope.context['a'] = 'A';
+          _.rootScope.context['b'] = 'B';
+        });
+
+        print(_.rootElement.outerHtml); // TODO - remove
+        ElementProbe divProbe = ngProbe(_.rootElement);
+
+        expect(divProbe.bindingExpressions).toEqual(['a']);
+
+        // The bindingExpressions should still be listed on the element probe for the text node.
+        ElementProbe textProbe = ngProbe(_.rootElement.firstChild);
+
+        expect(textProbe.bindingExpressions).toEqual(['a']);
+
+        ElementProbe spanProbe = ngProbe(_.rootElement.querySelector('span'));
+
+        expect(spanProbe.bindingExpressions).toEqual(['b']);
+      });
+
+      it('should not duplicate listed bindings', (TestBed _) {
+        _.compile('<div>hi {{a}}, you are {{a}}</div>');
+        _.rootScope.apply(() {
+          _.rootScope.context['a'] = 'A';
+        });
+
+        print(_.rootElement.outerHtml); // TODO - remove
+        ElementProbe probe = ngProbe(_.rootElement);
+
+        expect(probe.bindingExpressions.length).toBe(1);
+        expect(probe.bindingExpressions).toEqual(['a']);
+      });
     });
 
     describe('getTestability', () {
@@ -125,80 +163,77 @@ void main() {
         expect(js.context['ngProbe'].apply([ngtop])).toBeDefined();
       });
 
-      // Issue #1219
-      if (identical(1, 1.0) || !js.context['DART_VERSION'].toString().contains("version: 1.5.")) {
-        describe(r'testability', () {
+      describe(r'testability', () {
 
-          var testability;
+        var testability;
 
-          beforeEach(() {
-            testability = angular['getTestability'].apply([ngtop]);
-          });
+        beforeEach(() {
+          testability = angular['getTestability'].apply([ngtop]);
+        });
 
-          it('should be available from Javascript', () {
-            expect(testability).toBeDefined();
-          });
+        it('should be available from Javascript', () {
+          expect(testability).toBeDefined();
+        });
 
-          it('should expose allowAnimations', () {
-            allowAnimations(allowed) => testability['allowAnimations'].apply([allowed]);
-            expect(allowAnimations(false)).toEqual(true);
-            expect(allowAnimations(false)).toEqual(false);
-            expect(allowAnimations(true)).toEqual(false);
-            expect(allowAnimations(true)).toEqual(true);
-          });
+        it('should expose allowAnimations', () {
+          allowAnimations(allowed) => testability['allowAnimations'].apply([allowed]);
+          expect(allowAnimations(false)).toEqual(true);
+          expect(allowAnimations(false)).toEqual(false);
+          expect(allowAnimations(true)).toEqual(false);
+          expect(allowAnimations(true)).toEqual(true);
+        });
 
-          describe('bindings', () {
-            it('should find exact bindings', () {
-              // exactMatch should fail.
-              var bindingNodes = testability['findBindings'].apply(['introspection', true]);
-              expect(bindingNodes.length).toEqual(0);
-
-              // substring search (default) should succeed.
-              // exactMatch should default to false.
-              bindingNodes = testability['findBindings'].apply(['introspection']);
-              expect(bindingNodes.length).toEqual(1);
-              bindingNodes = testability['findBindings'].apply(['introspection', false]);
-              expect(bindingNodes.length).toEqual(1);
-
-              // and so should exact search with the correct query.
-              bindingNodes = testability['findBindings'].apply(["'introspection FTW'", true]);
-              expect(bindingNodes.length).toEqual(1);
-            });
-
-            _assertBinding(String query) {
-              var bindingNodes = testability['findBindings'].apply([query]);
-              expect(bindingNodes.length).toEqual(1);
-              var node = bindingNodes[0];
-              var probe = js.context['ngProbe'].apply([node]);
-              expect(probe).toBeDefined();
-              var bindings = probe['bindings'];
-              expect(bindings['length']).toEqual(1);
-              expect(bindings[0].contains(query)).toBe(true);
-            }
-
-            it('should find ng-bind bindings', () => _assertBinding('introspection FTW'));
-            it('should find attribute mustache bindings', () => _assertBinding('attrMustache'));
-            it('should find text mustache bindings', () => _assertBinding('textMustache'));
-          });
-
-          it('should find models', () {
+        describe('bindings', () {
+          it('should find exact bindings', () {
             // exactMatch should fail.
-            var modelNodes = testability['findModels'].apply(['my', true]);
-            expect(modelNodes.length).toEqual(0);
+            var bindingNodes = testability['findBindings'].apply(['introspection', true]);
+            expect(bindingNodes.length).toEqual(0);
 
             // substring search (default) should succeed.
-            modelNodes = testability['findModels'].apply(['my']);
-            expect(modelNodes.length).toEqual(1);
-            var divElement = modelNodes[0];
-            expect(divElement is DivElement).toEqual(true);
-            var probe = js.context['ngProbe'].apply([divElement]);
-            expect(probe).toBeDefined();
-            var models = probe['models'];
-            expect(models['length']).toEqual(1);
-            expect(models[0]).toEqual('myModel');
+            // exactMatch should default to false.
+            bindingNodes = testability['findBindings'].apply(['introspection']);
+            expect(bindingNodes.length).toEqual(1);
+            bindingNodes = testability['findBindings'].apply(['introspection', false]);
+            expect(bindingNodes.length).toEqual(1);
+
+            // and so should exact search with the correct query.
+            bindingNodes = testability['findBindings'].apply(["'introspection FTW'", true]);
+            expect(bindingNodes.length).toEqual(1);
           });
+
+          _assertBinding(String query) {
+            var bindingNodes = testability['findBindings'].apply([query]);
+            expect(bindingNodes.length).toEqual(1);
+            var node = bindingNodes[0];
+            var probe = js.context['ngProbe'].apply([node]);
+            expect(probe).toBeDefined();
+            var bindings = probe['bindings'];
+            expect(bindings['length']).toEqual(1);
+            expect(bindings[0].contains(query)).toBe(true);
+          }
+
+          it('should find ng-bind bindings', () => _assertBinding('introspection FTW'));
+          it('should find attribute mustache bindings', () => _assertBinding('attrMustache'));
+          it('should find text mustache bindings', () => _assertBinding('textMustache'));
         });
-      }
+
+        it('should find models', () {
+          // exactMatch should fail.
+          var modelNodes = testability['findModels'].apply(['my', true]);
+          expect(modelNodes.length).toEqual(0);
+
+          // substring search (default) should succeed.
+          modelNodes = testability['findModels'].apply(['my']);
+          expect(modelNodes.length).toEqual(1);
+          var divElement = modelNodes[0];
+          expect(divElement is DivElement).toEqual(true);
+          var probe = js.context['ngProbe'].apply([divElement]);
+          expect(probe).toBeDefined();
+          var models = probe['models'];
+          expect(models['length']).toEqual(1);
+          expect(models[0]).toEqual('myModel');
+        });
+      });
     });
   });
 }
